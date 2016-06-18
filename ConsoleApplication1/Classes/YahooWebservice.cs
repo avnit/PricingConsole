631656b99b15e76d66ebc41f14e9bcecc2fd6546
Avnit Bambah <Avnit Bambah@AVNITBAMBAH-PC>
2016-06-18 18:29:11 -0500

    Update to Yahoo Webservice Class

---
+++ ConsoleApplication1/Classes/YahooWebservice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackSholvesModelPricing.Classes
+{
+    class YahooWebservice
+    {
+        public string URL { get; set; }
+        public string StockName { get; set;}
+        public string Date { get; set; }
+        public string Path { get; set; }
+
+        protected string FileName()
+        {
+            return this.Path + "/" + Date + "_" + this.StockName + ".csv";
+        }
+
+
+        protected string WebServiceCall()
+        {
+            return "http://" + this.URL + "?stockName = " + this.StockName + "&Date=" + this.Date;
+        }
+
+        public void DownloadFile ()
+        {
+            string webserviceUrl = this.WebServiceCall();
+            string dayFile = this.FileName();
+            File.Create(dayFile);
+            WebClient WS = new WebClient();
+            WS.DownloadFile(webserviceUrl, dayFile);
+            Console.WriteLine(this.StockName + " Data downloaded successfully!");
+            WS.Dispose();
+        }
+
+    }
+}
