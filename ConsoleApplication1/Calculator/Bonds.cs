dfd4b1baba35d3946c9cd53f8e3d35b7ef6b6e17
Avnit Bambah <abambah@gmail.com>
2017-10-17 13:39:12 -0500

    Adding Bond calculation space

---
+++ ConsoleApplication1/Calculator/Bonds.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QLNet;
+
+namespace BlackSholvesModelPricing.Calculator
+{
+    class Bonds
+    {
+        private static string workManagerString; 
+
+
+    }
+}
